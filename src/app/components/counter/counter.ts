05e4777fcf2b381a7ad274515518b08d83607565
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-11-03 18:14:53 +0530

    Done Counter app

--- src/app/components/counter/counter.ts
+++ src/app/components/counter/counter.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -7,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './counter.scss',
 })
 export class Counter {
+
+  counterValue = signal(0);
+
+  //getting the current value using parenthesis
+  increment() {
+    // this.counterValue.set(this.counterValue () + 1);
+    //using update function get the latest value and then add one to it and combine them and return
+    this.counterValue.update((val) => val + 1);
+  }
+  decrement() {
+    // this.counterValue.set(this.counterValue () - 1);
+    this.counterValue.update((val) => val - 1);
+  }
+  reset() {
+    //  this.counterValue.set(0);
+    this.counterValue.set(0);
+  }
+  
 
 }
