40c8e78e3c9f3d053b8477524b7eba342d5efa93
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-11-04 20:46:50 +0530

    Createing Todo navigation using routerlink in header(single page application without reloading the whole Html)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,22 @@
 import { Routes } from '@angular/router';
 
 //we define our first route hire ? emty which means we are at the top or root
-export const routes: Routes = [{
-   path: '',
-   pathMatch: 'full',
-   loadComponent: () => {
-    return import('./home/home').then(
-        m => m.Home
-    )
-   },
-},
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () => {
+        return import('./home/home').then(
+            m => m.Home
+         )
+        },
+    },
+    {
+        path: 'todos',
+        loadComponent: () => {
+            return import('./todos/todos').then(
+                m => m.Todos
+            )
+        }
+    }
 ];
