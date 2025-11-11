b6bd1ef2c59f35c1d71387d053877391c3b80042
Maleesha Madhushanka <154691806+MaleeshaMAdhushanka@users.noreply.github.com>
2025-11-11 12:13:26 +0530

    Implement Todo item component with highlighting directive; refactor template and add toggle functionality

---
+++ src/app/directives/highlight-completed-todo.ts
@@ -0,0 +1,23 @@
+import { Directive, input, effect, inject, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlightCompletedTodo]'
+})
+export class HighlightCompletedTodo {
+
+  isCompleted = input(false);
+
+   el = inject(ElementRef);
+
+   styleEffect = effect(()=> {
+     if(this.isCompleted()) {
+       this.el.nativeElement.style.textDecoration = 'line-through';
+       this.el.nativeElement.style.backgroundColor = '#d3f9d8';
+       this.el.nativeElement.style.color = 'gray';
+     } else {
+       this.el.nativeElement.style.textDecoration = 'none';
+       this.el.nativeElement.style.color = 'black';
+     }
+  })
+
+}
--- src/app/directives/hightlight-completed-todo.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HightlightCompletedTodo } from './hightlight-completed-todo';
-
-describe('HightlightCompletedTodo', () => {
-  it('should create an instance', () => {
-    const directive = new HightlightCompletedTodo();
-    expect(directive).toBeTruthy();
-  });
-});
